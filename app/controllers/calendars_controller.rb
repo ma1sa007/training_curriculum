e5e00b6e9b891bb6376259c75f1f642a24b5df92
ma1sa007 <74030257+ma1sa007@users.noreply.github.com>
2020-11-20 10:48:57 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -18,7 +18,7 @@ class CalendarsController < ApplicationController
     params.require(:plan).permit(:date, :plan)
   end
 
-  def get＿Week
+  def get_Week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)'] 
     # wdays[1]
 
